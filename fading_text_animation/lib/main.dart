1a99269c4cd491f72e8662f62b2264fa822b0915
fernandocm9 <88690826+fernandocm9@users.noreply.github.com>
2025-02-26 13:37:18 -0500

    added gesture controll to fade text

--- fading_text_animation/lib/main.dart
+++ fading_text_animation/lib/main.dart
@@ -29,12 +29,15 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
     return Scaffold(
       appBar: AppBar(title: Text('Fading Text Animation')),
       body: Center(
-        child: AnimatedOpacity(
-          opacity: _isVisible ? 1.0 : 0.0,
-          duration: Duration(milliseconds: 1500),
-          curve: Curves.elasticInOut,
-          child: Text('Fade Animation App', style: TextStyle(fontSize: 24)),
-        ),
+        child: GestureDetector(
+          onTap: toggleVisibility,
+          child: AnimatedOpacity(
+            opacity: _isVisible ? 1.0 : 0.0,
+            duration: Duration(milliseconds: 1500),
+            curve: Curves.elasticInOut,
+            child: Text('Fade Animation App', style: TextStyle(fontSize: 24)),
+          ),
+        )
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: toggleVisibility,
